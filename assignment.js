0b482f87b1ace9a75a401bb2f01e2ec2317880f8
rajatjha9058 <rajatjha20070@gmail.com>
2025-01-21 21:37:54 +0530

    my assignment

---
+++ assignment.js
@@ -0,0 +1,100 @@
+
+// let mark=65;             //question no=1
+// if(mark>=90) {
+//     console.log("grade a");
+    
+// }else if(mark>80 && mark<89){
+// console.log("grade b");
+
+// }else if(mark>70 && mark<79){
+// console.log("grade c");
+
+// }else if(mark<70 && mark>=50){
+// console.log("grade d");
+
+// }else{
+//     console.log("grade f");
+    
+// }
+
+
+
+
+//  let a = 5;            //question no =2
+//   let b = 8;
+//      let c = 6;
+
+//      if(a==b && b==c && a==c){
+// console.log("Equilateral triangle");
+
+//      }else if(a==b ||b==c){
+// console.log("Isosceles triangle.");
+
+//      }else{
+//         console.log("Scalene triangle");
+        
+//      }
+
+
+
+// let  a=68;              //  question no=3
+
+// if(a%2==0){
+
+//     if(a>50){
+// console.log("Even and Large");
+
+//     }else{
+//         console.log("Even and Small");
+        
+//     }
+// }else if(a<30){
+// console.log("Odd and Small");
+
+// }else{
+//     console.log("Odd and Large");
+    
+// }
+
+
+//   for loop
+
+// for (let i = 1; i<=10 ;i++) {    //question no =1
+//     console.log(i*5);
+    
+// }
+
+
+// let n=6;                  // question no =2
+//  let sum= 0;
+
+// for (let i = 1; i <=n; i++) {
+//     sum+= i;
+//     console.log(sum);
+    
+// }
+
+
+// let a=4;             //question no =3
+
+//     for (let  i= 0; i <=a; i++) {        
+//         let rowstar="";
+    
+//         for (let  j= 1;  j<=i ; j++) {
+//             rowstar +=j;
+//         }
+//         console.log(rowstar);
+        
+//     }
+
+
+
+
+
+
+
+
+
+
+
+

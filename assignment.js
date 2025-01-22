1d6983ebdff73747f59d6b82a51dcb0b4c14a28c
rajatjha9058 <rajatjha20070@gmail.com>
2025-01-22 21:13:44 +0530

    my second assin

--- assignment.js
+++ assignment.js
@@ -2,7 +2,7 @@
 // let mark=65;             //question no=1
 // if(mark>=90) {
 //     console.log("grade a");
-    
+
 // }else if(mark>80 && mark<89){
 // console.log("grade b");
 
@@ -14,30 +14,32 @@
 
 // }else{
 //     console.log("grade f");
-    
+
 // }
 
 
 
 
 //  let a = 5;            //question no =2
-//   let b = 8;
-//      let c = 6;
-
+//   let b = 5;
+//      let c = -5;
+//      if(a<0||b<0||c<0){
+// console.log("Invalid");
+//      }else{
 //      if(a==b && b==c && a==c){
 // console.log("Equilateral triangle");
 
-//      }else if(a==b ||b==c){
+//      }else if(a==b&&c>0 ||b==c &&a>0 ||a==c&& b>0){
 // console.log("Isosceles triangle.");
 
 //      }else{
 //         console.log("Scalene triangle");
-        
+
 //      }
+//     }
 
 
-
-// let  a=68;              //  question no=3
+// let  a=27;              //  question no=3
 
 // if(a%2==0){
 
@@ -46,14 +48,14 @@
 
 //     }else{
 //         console.log("Even and Small");
-        
+
 //     }
 // }else if(a<30){
 // console.log("Odd and Small");
 
 // }else{
 //     console.log("Odd and Large");
-    
+
 // }
 
 
@@ -61,7 +63,7 @@
 
 // for (let i = 1; i<=10 ;i++) {    //question no =1
 //     console.log(i*5);
-    
+
 // }
 
 
@@ -71,7 +73,7 @@
 // for (let i = 1; i <=n; i++) {
 //     sum+= i;
 //     console.log(sum);
-    
+
 // }
 
 
@@ -79,12 +81,12 @@
 
 //     for (let  i= 0; i <=a; i++) {        
 //         let rowstar="";
-    
+
 //         for (let  j= 1;  j<=i ; j++) {
 //             rowstar +=j;
 //         }
 //         console.log(rowstar);
-        
+
 //     }
 
 
@@ -92,9 +94,108 @@
 
 
 
+// for (let i = 0; i <= 5; i++) {              //    // question=1   
+
+//     let rowstar = "";
+
+//     for (let j = 0; j <= 5-i ; j++) {
+//         rowstar += ""
+//     }
+//     for (let j = 0; j < i; j++) {
+//         rowstar += "*"
+//     }
+//     console.log(rowstar);
+
+// }
+
+
+// for (let i = 0; i <=5; i++) {            //question no =2
+
+//     let star=""
+//     for (let j = 0; j <=i; j++) {
+
+//          star =star+"";     
+//         }
+//         for (let j = 0; j <5-i; j++) {
+
+//             star+="*";
+//     }
+
+//     console.log(star);
+
+// }
+
+
+
+
+// for (let i = 1; i <=9; i+=2) {            //question no =3
+
+//         let star=""
+//         for (let j = 9; j>=i; j-=2) {
+
+//              star =star+" ";     
+//             }
+//             for (let k= 1 ; k <=i; k++) {
+
+//                 star+="*";
+//         }
+
+//         console.log(star);
+
+//     }
+
+
+
+// let a=5;                         // question no=4
+// for (let i = 0; i <=a; i++) {
+//     let star="";
+//     for (let j = 1; j <=i ;j++){
+//         star+=j
+//     }
+//     console.log(star);
+
+// }
+
+
+
+// let a=5;                    // question no =5
+
+// for (let i = 0; i <a; i++) {         
+//     let b="";
+//     for (let j = 5; j > i; j--) {
+//         b +=j
+//     }
+//     console.log(b);
+
+// }
 
 
 
 
 
 
+// let a = 9;
+
+// for (let i = 1; i <= a; i += 2) {            //question no =6
+//     let star = ""
+
+//     for (let j = a; j >= i; j -= 2) {
+
+//         star = star + " ";
+//     }
+//     for (let k = 1; k <= i; k++) {
+//         star += "*";
+//     }
+//     console.log(star);
+// }
+// for (let i = a - 2; i >= 1; i -= 2) {
+//     let row = "";
+//     for (let j = a; j >= i; j -= 2) {
+//         row += " ";
+//     }
+//     for (let k = 1; k <= i; k++) {
+//         row += "*";
+
+//     }
+//     console.log(row);
+// }

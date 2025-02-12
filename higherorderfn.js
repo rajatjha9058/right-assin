cf56c27fa8e4e3b5fdda00cb8a0cb52147cb309d
rajatjha9058 <rajatjha20070@gmail.com>
2025-02-12 21:43:38 +0530

    my task

---
+++ higherorderfn.js
@@ -0,0 +1,154 @@
+
+  
+//  //filter ke question 
+
+// let arr=[1,2,3,4,5,6]          // //question=1
+// function filter1() {
+// let result=arr.filter((ele)=>ele%2==0)  
+// return result  
+// }
+// console.log(filter1());
+
+
+ 
+// let arr=[-3,4,-1,6,-7,9]         // // question =2
+// function filter2(arr) {
+// let result=arr.filter((ele)=>ele>0)    
+// return result
+// }
+// console.log(filter2(arr));
+
+
+
+
+// let arr =["apple", "banana", "cat", "strawberry"];   // // question 3
+// function filter3(arr) {
+//     let result=arr.filter((ele)=>ele.length>5)
+//     return result
+// }
+
+// console.log(filter3(arr));
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];           // // question=4
+// function isPrime(num) {
+//     if (num <= 1) return false; 
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) return false; 
+//     }
+//     return true;
+// }
+// function prime(arr) {
+//     let primes = arr.filter(isPrime);
+//     console.log(primes);
+// }
+// prime(arr);
+
+
+
+// let arr=[{name: "Alice", age: 17}, {name: "Bob", age: 20}];    // // question =5
+// function filter5(arr) {
+//     let result=arr.filter((ele)=>ele.age>18)
+// return result
+// }
+// console.log(filter5(arr));
+
+
+
+
+// // map ke question
+
+// let arr=[1,2,3]         // // question=1
+// function map1(arr) {
+//     let result=arr.map((ele)=>ele*2)
+//     return result
+// }
+// console.log(map1(arr));
+
+
+// let arr=[0,25,100]         // // question=2
+// function temperatures(arr) {
+//     let result=arr.map((ele)=>(ele*9/5)+32)
+//     return result
+// }
+
+// console.log(temperatures(arr));
+
+
+// let arr=[{name: "Alice", age: 25}, {name: "Bob", age: 30}]    // // question=3
+// function map3(arr) {
+//     let result=arr.map((ele)=>ele.name )
+//     return result
+// }
+
+// console.log(map3(arr));
+
+
+// let  arr=["hello", "world"]     // // question=4
+// function map4(arr) {
+//     let result=arr.map((ele)=>ele.toUpperCase())
+//     return result
+// }
+// console.log(map4(arr));
+
+
+// let arr=[2,3,4]           // //question=5
+// function map5(arr) {
+//     let result=arr.map((ele)=>ele*ele)
+//     return result
+    
+// }
+
+// console.log(map5(arr));
+
+
+
+// // foreach ke question 
+
+
+// let arr=["apple", "banana", "cherry"];     // // question =1
+// function foreach1(arr) {
+//  arr.forEach((ele)=>console.log(ele))
+// }
+// foreach1(arr)
+
+
+// let arr=[1,2,3,4]       // // question=2
+// let sum=0
+// function foreach2(arr) {
+//     arr.forEach((ele)=>console.log(sum+=ele) )
+// }
+// foreach2(arr)
+
+
+
+// let arr=[1,2,3,4,6]        // // question =3
+// let count=0
+// function foreach3(arr) {
+//     arr.forEach((ele)=> {if (ele%2==0) {
+//         count++;   
+//     }}) 
+//     console.log(count);
+// }
+// foreach3(arr)
+
+
+
+// let arr=[{item: "apple", price: 100}, {item: "banana", price: 50}]    // //    question=4
+// function discount(arr) {
+//     arr.forEach((ele)=>{
+//         let a=ele.price-ele.price*0.1;
+//         console.log({item:ele.item,price:a});
+        
+//     })
+    
+// }
+// discount(arr)
+
+
+
+// let arr=["hello", "world"];           // // question=5
+// function foreach5(arr) {
+//     arr.forEach((ele)=>console.log(ele+"!"))
+// }
+// foreach5(arr)
